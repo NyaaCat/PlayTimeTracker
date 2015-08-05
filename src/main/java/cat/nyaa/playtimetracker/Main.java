4515711d5030b818766429e73c123d2dcca7324e
Recursive G <g.recursive@gmail.com>
2015-08-05 12:30:24 +0800

    basic frame

---
+++ src/main/java/cat/nyaa/playtimetracker/Main.java
@@ -0,0 +1,27 @@
+package cat.nyaa.playtimetracker;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return super.onCommand(sender, command, label, args);
+    }
+}
