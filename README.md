4f2bd2f68d46694d2a068e2dd7e7fa771c44aa51
dwcarrot <dw1064carota@outlook.com>
2024-08-16 00:44:28 +0800

    add github actions & port to 1.21

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # PlayTimeTracker
-Playtime tracking plugin for Bukkit/Spigot
+Playtime tracking plugin for ~~Bukkit/Spigot~~ PaperMC
+
+
+## Build locally
+
+1. Clone the repository
+
+2. [Acquire a Github personal access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens) with at least read:packages scope.
+
+3. Modify the `gradle.properties` file
+
+   ```
+   gpr.user=<GITHUB_USERNAME>
+   gpr.key=<GITHUB_TOKEN>
+   ```
+4. Run the build and publish (locally) with maven.
+
+5. **When committing changes, make sure to remove what you add in step 3. so that `GITHUB_TOKEN` is not exposed.**
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,45 +6,31 @@ plugins {
 // = = =
 
 val pluginName = "PlayerTimeTracker"
-val majorVersion = 0
-val minorVersion = 8
-
-val paperApiName = "1.20.4-R0.1-SNAPSHOT"
-
-// = = =
-
-// for Jenkins CI
-val buildNumber = System.getenv("BUILD_NUMBER") ?: "local"
-val mavenDirectory: String =
-    System.getenv("MAVEN_DIR")
-        ?: layout.buildDirectory.dir("repo").get().asFile.absolutePath
-val javaDocDirectory: String =
-    System.getenv("JAVADOC_DIR")
-        ?: layout.buildDirectory.dir("javadoc").get().asFile.absolutePath
+val paperApiName = "1.21.1-R0.1-SNAPSHOT"
 
 // Version used for distribution. Different from maven repo
 group = "cat.nyaa"
-//archivesBaseName = "${pluginNameUpper}-mc$minecraftVersion"
-version =
-    "$majorVersion.$minorVersion.$buildNumber"
+version = "0.9"
 
 java {
-    // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 8 installed for example.
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://papermc.io/repo/repository/maven-public/")
-    } //paper
+    maven { url = uri("https://papermc.io/repo/repository/maven-public/") } //paper
     maven { url = uri("https://libraries.minecraft.net") } // mojang
     maven { url = uri("https://repo.essentialsx.net/releases/") } // essentials
-    maven { url = uri("https://ci.nyaacat.com/maven/") } // nyaacat
+    // maven { url = uri("https://ci.nyaacat.com/maven/") } // nyaacat
     maven {
-        url =
-            uri("https://repo.extendedclip.com/content/repositories/placeholderapi/")
-    } // placeholderapi
+        url = uri("https://maven.pkg.github.com/NyaaCat/NyaaCore")
+        credentials {
+            username = System.getenv("GITHUB_MAVEN_USER") ?: project.findProperty("gpr.user").toString()
+            password = System.getenv("GITHUB_MAVEN_TOKEN") ?: project.findProperty("gpr.key").toString()
+        }
+    } // NyaaCore
+    maven { url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/") } // placeholderapi
 }
 
 dependencies {
@@ -57,58 +43,59 @@ dependencies {
     compileOnly("org.xerial:sqlite-jdbc:3.42.0.0")
     compileOnly("com.zaxxer:HikariCP:5.0.1")
     // other nyaa plugins
-    compileOnly("cat.nyaa:nyaacore:9.3.4-1.20.4")
+    compileOnly("cat.nyaa:nyaacore:9.4")
 }
 
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(getComponents()["java"])
-            afterEvaluate {
-                artifactId = pluginName.lowercase()
-                groupId = "$group"
-                version =
-                    "$majorVersion.$minorVersion.$buildNumber-${
-                        getMcVersion(
-                            paperApiName
-                        )
-                    }"
-            }
+            artifactId = pluginName.lowercase()
+            groupId = group.toString()
+            version = project.version.toString()
         }
     }
     repositories {
         maven {
-            name = "nyaaMaven"
-            url = uri(mavenDirectory)
+            name = "maven-publish"
+            url = uri(System.getenv("MAVEN_URL") ?: layout.buildDirectory.dir("repo"))
+            val mavenUsername = System.getenv("MAVEN_PUBLISH_USERNAME")
+            val mavenPassword = System.getenv("MAVEN_PUBLISH_PASSWORD")
+            if(mavenUsername != null && mavenPassword != null) {
+                credentials {
+                    username = mavenUsername
+                    password = mavenPassword
+                }
+            }
         }
     }
 }
 
-/*
-reobfJar {
-     // This is an example of how you might change the output location for reobfJar. It's recommended not to do this
-     // for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
-     outputJar.set(layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar"))
-}
-*/
-
 
 tasks {
+
+    withType<ProcessResources> {
+        val newProperties = project.properties.toMutableMap()
+        newProperties["api_version"] = getMcVersion(paperApiName, 2)
+        filesMatching("plugin.yml") {
+            expand(newProperties)
+        }
+    }
+
     compileJava {
         options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
 
         // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
         // See https://openjdk.java.net/jeps/247 for more information.
-        options.release.set(17)
+        options.release.set(21)
     }
     javadoc {
         with((options as StandardJavadocDocletOptions)) {
-            options.encoding =
-                Charsets.UTF_8.name() // We want UTF-8 for everything
-            links("https://docs.oracle.com/en/java/javase/17/docs/api/")
+            options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+            links("https://docs.oracle.com/en/java/javase/21/docs/api/")
             links("https://guava.dev/releases/21.0/api/docs/")
             links("https://ci.md-5.net/job/BungeeCord/ws/chat/target/apidocs/")
-            links("https://jd.papermc.io/paper/1.20/")
+            links("https://jd.papermc.io/paper/1.21/")
             options.locale = "en_US"
             options.encoding = "UTF-8"
             (options as StandardJavadocDocletOptions).addBooleanOption(
@@ -131,6 +118,12 @@ tasks {
 
 }
 
-private fun getMcVersion(apiNameString: String): String {
-    return apiNameString.split('-')[0]
+private fun getMcVersion(apiNameString: String, parts: Int = -1): String {
+    val mcVersion = apiNameString.split('-')[0]
+    val splitInput = mcVersion.split('.')
+    return if (parts > 0 && parts < splitInput.size) {
+        splitInput.take(parts).joinToString(".")
+    } else {
+        mcVersion
+    }
 }
